e10ea3fa1d76d7842879dbe8269cc50ea71c8290
Rafael Reis <rafael.araujoreis@hotmail.com>
2021-05-07 20:48:10 -0300

    Remove comment

--- scripts/script.js
+++ scripts/script.js
@@ -3,11 +3,9 @@ const modal = form.parentElement;
 
 const Modal = {
   open() {
-    console.log('aqui');
     modal.classList.add('active');
   },
   close() {
-    console.log('ali');
     modal.classList.remove('active');
 
   }
