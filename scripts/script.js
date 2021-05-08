6fda802cc0e9a68f397f5cb162f4b5f15475c187
Rafael Reis <rafael.araujoreis@hotmail.com>
2021-05-07 21:43:09 -0300

    transactions mock

--- scripts/script.js
+++ scripts/script.js
@@ -8,5 +8,23 @@ const Modal = {
   close() {
     modal.classList.remove('active');
 
+const transactions = [
+  {
+    id: 1,
+    description: 'Desenvolvimento',
+    amount: 1200000,
+    date: '13/04/2021'
+  },
+  {
+    id: 2,
+    description: 'Conta de luz',
+    amount: -60000,
+    date: '13/04/2021'
+  },
+  {
+    id: 3,
+    description: 'Terreno',
+    amount: -100000,
+    date: '13/04/2021'
   }
 };
